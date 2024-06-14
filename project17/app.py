0b7e4856ad2d5dfd01f4a2a974f3bad23f6ebb27
Bengaluru Tech Developers <techsaadhana@gmail.com>
2024-06-14 19:05:49 +0200

    added AI SaaS apps based that classifies using diff. classifiers

---
+++ project17/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import numpy as np
+import joblib
+from sklearn.datasets import load_iris
+
+model = joblib.load("iris_model.pkl")
+
+dataset = load_iris()
+
+feature_names = dataset.feature_names
+
+st.markdown("<span style='text-align: center; font-size: 44px;font-weight: bold;'>Iris Classification using LinearDiscriminantAnalysis - Metric Coders <span>", unsafe_allow_html=True)
+
+st.header("Input Features")
+
+
+sepal_length = st.slider("Sepal Length (cm)", min_value=4.0, max_value=8.0, value=5.0, step=0.1)
+sepal_width = st.slider("Sepal Width (cm)", min_value=4.0, max_value=8.0, value=5.0, step=0.1)
+petal_length = st.slider("Petal Length (cm)", min_value=1.0, max_value=7.0, value=3.0, step=0.1)
+petal_width = st.slider("Petal Width (cm)", min_value=0.1, max_value=2.5, value=1.1, step=0.1)
+
+
+features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+prediction = model.predict(features)
+
+predicted_species = dataset.target_names[prediction][0]
+
+st.markdown(f"<h3 style='color: green; text-align: center;'>The predicted species is: **{predicted_species}**</h3>", unsafe_allow_html=True)
---
+++ project21/ml_model.py
@@ -0,0 +1,20 @@
+import joblib
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+
+dataset = load_iris()
+X, y = dataset.data, dataset.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+print(f"Accuracy:{accuracy_score(y_test, y_pred)}")
+
+joblib.dump(clf, "iris_model.pkl")
+
+
