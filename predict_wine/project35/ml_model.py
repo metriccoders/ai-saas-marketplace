a94c789e5ca096e78121f56f7785e405213030ba
Bengaluru Tech Developers <techsaadhana@gmail.com>
2024-06-14 20:23:08 +0200

    added saas apps that can classify wine

---
+++ predict_wine/project35/ml_model.py
@@ -0,0 +1,20 @@
+import joblib
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import RidgeClassifier
+from sklearn.metrics import accuracy_score
+
+
+dataset = load_wine()
+X, y = dataset.data, dataset.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = RidgeClassifier()
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+print(f"Accuracy:{accuracy_score(y_test, y_pred)}")
+
+joblib.dump(clf, "wine_model.pkl")
+
+
